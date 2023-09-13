6213d0a07773dab2522f7eea5cf1508dcadff5f2
zak-farrington <fritz@hax-studios.net>
2023-09-13 13:54:30 -0500

    simple templates from LeetCode

---
+++ Templates/BinaryTree.DFS.Recursive.js
@@ -0,0 +1,12 @@
+let dfs = root => {
+    if (!root) {
+        return;
+    }
+
+    let ans = 0;
+
+    // do logic
+    dfs(root.left);
+    dfs(root.right);
+    return ans;
+}
---
+++ Templates/TwoPointers.OneInput.js
@@ -0,0 +1,14 @@
+let fn = arr => {
+    let left = 0, ans = 0, right = arr.length - 1;
+
+    while (left < right) {
+        // do some logic here with left and right
+        if (CONDITION) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return ans;
+}
